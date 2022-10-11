5bfd2767b08e09e1552ee8634a981f77cde3e1db
Knosence <nadario.seays@gmail.com>
2022-10-11 17:54:38 -0400

    day 6 done

---
+++ Week2-Day2/src/com/skillstorm/HoneyBadger.java
@@ -0,0 +1,20 @@
+package com.skillstorm;
+
+// also inherits from Animal
+public class HoneyBadger extends Animal{
+
+	public boolean isUnstoppable = true;
+	
+	public HoneyBadger() {
+		super();
+	}
+	
+	// overriding
+	public void speak() {
+		System.out.println("Razzle-frazzle... take that, honeybees!");
+	}
+	
+	public static void drink() {
+		System.out.println("Glub, glub... HoneyBadger drink bee souls!");
+	}
+}
